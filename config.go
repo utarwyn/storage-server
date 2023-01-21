634ab9b492d42d64a015fc886991725cafe25709
utarwyn <maxime.malgorn@laposte.net>
2023-01-21 13:38:06 +0100

    docs: update LICENSE

--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Maxime Malgorn. All rights reserved.
+// Copyright 2023 Maxime Malgorn. All rights reserved.
 // Use of this source code is governed by a MIT-style.
 // The license can be found in the LICENSE file.
 
